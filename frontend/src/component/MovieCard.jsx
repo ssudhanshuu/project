2a8807b02ed679da2f1d7d5b4433a0f4f3ad9e41
Sudhanshu <167232298+ssudhanshuu@users.noreply.github.com>
2025-11-18 20:08:52 +0530

    Update MovieCard.jsx

--- frontend/src/component/MovieCard.jsx
+++ frontend/src/component/MovieCard.jsx
@@ -11,7 +11,7 @@ export default function MovieCard() {
   const [allMovies, setAllMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const { query } = useSearch();
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL || "https://project-1-ahno.onrender.com";
 
   useEffect(() => {
     const fetchMovies = async () => {
